8024607482dbe943a92dca0864bbb6efb9a0c2fe
archeryang57 <archeryang@me.com>
2021-03-08 09:57:15 +0800

    Add comments

--- ball_chaser/ball_chaser/process_image_cv.py
+++ ball_chaser/ball_chaser/process_image_cv.py
@@ -15,14 +15,12 @@ from sensor_msgs.msg import CompressedImage
 from geometry_msgs.msg import Twist
 
 class ProcessImageCV(Node):
-    right_state = 0
-    left_state = 0
     
     def __init__(self):
         super().__init__("process_image")
         self.get_logger().info("Hello process_image")
 
-        # init the OpenCV dependance environment
+        # 物件變數初始化
         self.bridge = cv_bridge.CvBridge()
         # self.greenLower = (29, 86, 6)
         self.greenLower = (31, 120, 20)
@@ -38,15 +36,13 @@ class ProcessImageCV(Node):
         # self.create_subscription(Image, "/image_raw", self.callback_func, 10)
         self.create_subscription(CompressedImage, "/image_raw/compressed", self.callback_func, 10)
 
+        # 將處理過的 Image publish 以供檢視
         self.publichser_ = self.create_publisher(Image, "ball_image", 10)
 
+        # 因為透過 Service Latancy 很嚴重, 因此直接將動作publish給 TB3, 不透過 Service
         self.motor_command_publisher = self.create_publisher(Twist, "/cmd_vel", 10)
 
-
     def drive_robot(self, lin_x,  ang_z):
-        # TODO: Request a service and pass the velocities to it to drive the robot
-        # self.get_logger().info(f"Driving the bot forward({lin_x}, turn({ang_z})")
-
         motor_command = Twist()
         motor_command.linear.x = lin_x
         motor_command.angular.z = ang_z
@@ -55,51 +51,54 @@ class ProcessImageCV(Node):
 
     # * 建立 call back function, 撰寫執行動作
     def callback_func(self, msgimg: CompressedImage):
+        # 若 msg 是 raw image, 直接轉為 CV2 格式
         # frame = self.bridge.imgmsg_to_cv2(msgimg, "bgr8")
+        
+        # 將壓縮影像解壓, 轉為 CV2 格式
         np_arr = np.fromstring(msgimg.data.tostring(), np.uint8)
         frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
+        # 畫面做左右鏡射處理
         frame = cv2.flip(frame, 1)
 
-        # if we are viewing a video and we did not grab a frame,
-        # then we have reached the end of the video
-        if frame is None:
-            return 1
-
+        # 取得畫面的高度及寬度
         # height = msgimg.height # image height, that is, number of rows  640
         # width = msgimg.width # image width, that is, number of columns  480
+        
+        # frame.shape example:  ( 1080, 1920, 3 ) ( height, width, color )
         height, width = frame.shape[:2]
+        
+        # 畫面中心點
         centerX = width /2
         centerY = height /2
 
-        # resize the frame, blur it, and convert it to the HSV
-        # color space
+        # 影像模糊化
         blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        
+        # 轉為 HSV 色彩空間
         hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-        # construct a mask for the color "green", then perform
-        # a series of dilations and erosions to remove any small
-        # blobs left in the mask
+        # 取出綠色部份做為影像遮罩, 以供 findConturs 使用
         mask = cv2.inRange(hsv, self.greenLower, self.greenUpper)
+        
+        # 將影像遮罩做腐蝕及膨脹處理, 以去掉影像毛邊.
         # mask = cv2.erode(mask, None, iterations=2)
         # mask = cv2.dilate(mask, None, iterations=2)
 
-        # find contours in the mask and initialize the current
-        # (x, y) center of the ball
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # 取得 mask中的影像輪廓
+        cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
+
+        # 初始化變數.
         center = (centerX + 0.0 , centerY + 0.0)
-        
         ((x, y), radius) = ((0.0, 0.0), 0.0) 
         # (dX, dY) = (0.0, 0.0)
         # M = 0
 
-        # only proceed if at least one contour was found
-        if len(cnts) > 0:
-            # find the largest contour in the mask, then use
-            # it to compute the minimum enclosing circle and
-            # centroid
+       if len(cnts) > 0:
+            # 取得最大的輪廓
             c = max(cnts, key=cv2.contourArea)
+            # 取得包含輪廓的最小圓形外框((中心點x,y), 半徑)
             ((x, y), radius) = cv2.minEnclosingCircle(c)
 
             # only proceed if the radius meets a minimum size
@@ -108,13 +107,16 @@ class ProcessImageCV(Node):
                 if M["m00"] != 0:
                     center = ( int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-                # draw the circle and centroid on the frame,
-                # then update the list of tracked points
+                # 畫圓形外框
                 cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+
+                # 畫中心點
                 # cv2.circle(frame, center, 5, (0, 0, 255), -1)
+                
+                # 將物件中心點存入 pts
                 # self.pts.appendleft(center)
                 
-        # # loop over the set of tracked points
+        # # 處理追蹤點tracked points, 以及畫出軌跡線
         # for i in np.arange(1, len(self.pts)):
         #     # if either of the tracked points are None, ignore them
         #     if self.pts[i - 1] is None or self.pts[i] is None:
@@ -150,33 +152,44 @@ class ProcessImageCV(Node):
         #     (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
         #     0.35, (0, 0, 255), 1)
 
+        # 將處理過的圖形 publish 供檢視
         self.publichser_.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
         self.counter += 1     
 
-        # ball found, calculate forces.
-        if radius > 0:
+        # 若有找到符合物件, 則計算驅動力
+        if radius >= 5:
             # self.maxForwardForce = 0.21
             # self.maxTurnForce = 2.6
+            
+            # 
             forwardForce = self.maxForwardForce - ( ( radius * 2 ) / width * self.maxForwardForce )
+            # 若計算值小於 0, 前進力量就設為 0
             forwardForce = 0.0 if forwardForce < 0.0 else forwardForce
 
+            # 計算物件是否在中心區域( 物件中心-畫面中心 +- 20 點 )
             inCenterZone = True if abs(center[0]-centerX) < 20 else False
-            if inCenterZone:
+            if inCenterZone:  # 在中心區域不做旋轉
                 turnForce = 0.0
             else:
+                # 計算旋轉力量 ( (物件中心-畫面中心) / 畫面寬度 * 2 ) * 最大旋轉力量
+                # (物件在最旁時除以寬度約是 0.5, 乘 2 時會是 1, 物件中心在左右時, 正好會取得左右轉所對應的正負值 )
                 turnForce = ( (center[0] - centerX) / width * 2 ) * (self.maxTurnForce-0.5)
+                
+                # 保存旋轉力數值, 供物件跑出畫面時持續尋找
                 self.direction = turnForce
             
+            # 送出驅動力量數值
             self.drive_robot (forwardForce, turnForce)
 
             self.get_logger().info(f"radius:({round(radius,2)}), Ball Center:({center}), Forward/Turn: {round(forwardForce,2)}/{round(turnForce,2)}")
-        else:
+        else: # 沒找到物件
+            # 依保存的旋轉力值持續旋轉
             self.drive_robot (0.0, self.direction)
-            # keep searching direction for a while
+            
+            # 慢慢減少旋轉力值, 直到 <= 0.2 時停止旋轉
             self.direction = self.direction - 0.01 if self.direction > 0 else self.direction + 0.01
             if abs(self.direction) <= 0.2:
                 self.direction = 0.0
-
 
         return 0
 
